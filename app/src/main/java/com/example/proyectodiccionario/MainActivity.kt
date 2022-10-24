193cc96d8606f5e72faec90eb92351833b5f3311
Jeffrey Loaiza Quiceno <jeffreyloaiza92@hotmail.com>
2022-10-23 22:09:29 -0500

    Creacion de barra navegacion y loggin

--- app/src/main/java/com/example/proyectodiccionario/MainActivity.kt
+++ app/src/main/java/com/example/proyectodiccionario/MainActivity.kt
@@ -4,14 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private var edtUserName: EditText? =null
+    private var edtPassword: EditText? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edtUserName = findViewById(R.id.textUser)
+        edtPassword = findViewById(R.id.textPassword)
     }
     fun onLogin(btn_join: View){
-        val intento= Intent(this, TranslateActivity::class.java)
-        startActivity(intento)
+        val  userLayout= findViewById<TextInputLayout>(R.id.outlinedTexUser)
+        val passwordLayout = findViewById<TextInputLayout>(R.id.outlinedTextPassword)
+        if (edtUserName!!.text.toString() =="jeff@gmail.com"){
+            if (edtPassword!!.text.toString() == "1234") {
+                userLayout.error = null
+                passwordLayout.error = null
+                val intento= Intent(this, TranslateActivity::class.java)
+                startActivity(intento)
+            }
+            else{
+                passwordLayout.error = getString(R.string.passwordError)
+                userLayout.error = null
+            }
+        }
+        else{
+            userLayout.error = getString(R.string.userError)
+            passwordLayout.error = null
+        }
+
     }
 }
--- app/src/main/java/com/example/proyectodiccionario/TranslateActivity.kt
+++ app/src/main/java/com/example/proyectodiccionario/TranslateActivity.kt
@@ -1,11 +1,69 @@
 package com.example.proyectodiccionario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class TranslateActivity : AppCompatActivity() {
+    private var idioma: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
+        var bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        setSupportActionBar(findViewById(R.id.bottomAppBar))
+        bottomAppBar.setNavigationOnClickListener {
+            val intento= Intent(this, TranslateActivity::class.java)
+            startActivity(intento)
+        }
+
     }
+
+    fun cambiarIdioma(btnChange: View){
+        var botonIngles = findViewById<Button>(R.id.containerButtonEn)
+        var botonEspanol = findViewById<Button>(R.id.containerButtonEs)
+        if (idioma == false){
+            this.idioma=true
+            botonIngles.setText(R.string.txt_es)
+            botonEspanol.setText(R.string.txt_en)
+        }
+        else{
+            this.idioma=false
+            botonIngles.setText(R.string.txt_en)
+            botonEspanol.setText(R.string.txt_es)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_app_bar,menu)
+        return  super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.favourite -> {
+            val intento= Intent(this, FavoritosActivity::class.java)
+            startActivity(intento)
+            true
+        }
+
+        R.id.conjugation -> {
+            val intento= Intent(this, ConjugacionActivity::class.java)
+            startActivity(intento)
+            true
+        }
+
+        R.id.more -> {
+            // Handle more item (inside overflow menu) press
+            true
+        }
+        else -> false
+    }
+
 }
+
+
